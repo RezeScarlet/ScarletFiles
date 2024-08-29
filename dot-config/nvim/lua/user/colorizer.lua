fc28c402f2f00dead7515ba9b3ce944fbd23c2f3
RezeScarlet <gui171204@gmail.com>
2024-08-29 12:39:41 -0300

    Colorizer for all files

--- dot-config/nvim/lua/user/colorizer.lua
+++ dot-config/nvim/lua/user/colorizer.lua
@@ -5,16 +5,16 @@ local M = {
 
 function M.config()
   require("colorizer").setup {
-    filetypes = {
-      "typescript",
-      "typescriptreact",
-      "javascript",
-      "javascriptreact",
-      "css",
-      "html",
-      "astro",
-      "lua",
-    },
+    -- filetypes = {
+    --   "typescript",
+    --   "typescriptreact",
+    --   "javascript",
+    --   "javascriptreact",
+    --   "css",
+    --   "html",
+    --   "astro",
+    --   "lua",
+    -- },
     user_default_options = {
       names = false,
       rgb_fn = true,
