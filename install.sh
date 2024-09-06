7057f9d401c01b906d0fc32438582115e8016778
RezeScarlet <gui171204@gmail.com>
2024-09-06 09:56:29 -0300

    Created

---
+++ install.sh
@@ -0,0 +1,16 @@
+if ! command -v yay
+then
+  pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+  cd ..
+  rm -r yay
+fi
+if ! ls ~/.local/share  | grep "omf"
+then
+  curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+fi
+yay -Syuq --noconfirm --needed stow git neovim github-cli fish firefox yazi zoxide p7zip i3-wm autotiling polybar picom-git flameshot ttf-cascadia-code-nerd ttf-nerd-fonts-symbols-mono zscroll-git dunst rofi-power-menu i3lock xss-lock texlive zathura
+cd ~/ScarletFiles
+stow --dotfiles . 
