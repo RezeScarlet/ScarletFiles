af7dae87ebd8485f155580cdf7dde86c5ba66ec2
RezeScarlet <gui171204@gmail.com>
2024-11-07 18:33:42 -0300

    messing with jdtls

--- dot-config/nvim/lua/user/nvim-java.lua
+++ dot-config/nvim/lua/user/nvim-java.lua
@@ -2,17 +2,39 @@ local M = {
   "nvim-java/nvim-java",
   lazy = false,
 }
--- require('java').setup()
-require("lspconfig").jdtls.setup {
-  cmd = { "jdtls", "-data", '~/Desktop/Computer-Science/Data Structures/ProjetoESDArvores-master/' },
-  settings = {
-    java = {
-      project = {
-        referencedLibraries = {
-          "lib/JSGE-v1.1.0/jsge.jar", -- substitua pelo caminho correto do arquivo .jar
+
+jdtls = function()
+  require("java").setup() {
+    root_markers = {
+      "settings.gradle",
+      "settings.gradle.kts",
+      "pom.xml",
+      "build.gradle",
+      "mvnw",
+      "gradlew",
+      "build.gradle",
+      "build.gradle.kts",
+      ".git",
+    },
+    sources = {
+      organizeImports = {
+        starThreshold = 9999,
+        staticStarThreshold = 9999,
+      },
+    }
+  }
+
+  require("lspconfig").jdtls.setup {
+    cmd = { "jdtls", "-data", "/home/rezescarlet/Desktop/Non-Linear-Structures/" },
+    settings = {
+      java = {
+        project = {
+          referencedLibraries = {
+            "lib/JSGE-v1.1.0/jsge.jar", -- substitua pelo caminho correto do arquivo .jar
+          },
         },
       },
     },
-  },
-}
+  }
+end
 return M
