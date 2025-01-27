0ed509f16d4cf790145866e142eb987f7a95cf08
RezeScarlet <gui171204@gmail.com>
2025-01-27 18:48:41 -0300

    Updated install script

--- install.sh
+++ install.sh
@@ -7,10 +7,48 @@ then
   cd ..
   rm -r yay
 fi
-if ! ls ~/.local/share  | grep "omf"
+
+
+# TODO ly install, grub theme, laptop app list
+echo "========================="
+echo "=== App Installation  ==="
+echo "========================="
+
+echo "Install i3 rice apps? [Y/n]: "
+read -n 1 ans
+if ans != "n" || ans != "N"
 then
-  curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+  yay -Syuq --noconfirm --needed  i3-wm autotiling polybar picom-git zscroll-git dunst rofi-power-menu i3lock xss-lock 
 fi
-yay -Syuq --noconfirm --needed stow git neovim github-cli fish zen-browser-bin yazi-nightly-bin zoxide p7zip i3-wm autotiling polybar picom-git flameshot ttf-cascadia-code-nerd ttf-nerd-fonts-symbols-mono zscroll-git dunst rofi-power-menu i3lock xss-lock texlive zathura
+
+echo "Install personal apps? [Y/n]"
+read -n 1 ans
+if ans != "n" || ans != "N"
+then
+  # Apps for both setups
+  yay -Syuq --noconfirm --needed audiorelay vesktop-bin vlc ncpamixer bluetui thunar qbittorrent btop obsidian ttf-cascadia-code-nerd ttf-nerd-fonts-symbols-mono stow git neovim github-cli fish zen-browser-bin yazi-nightly-bin zoxide p7zip flameshot texlive zathura ly lazygit
+  systemctl enable ly.service
+
+  # Fish theme
+  if ! ls ~/.local/share  | grep "omf"
+  then
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+  fi
+
+  echo "Install desktop apps? [Y/n]: "
+  read -n 1 ans
+  if ans != "n" || ans != "N"
+  then
+   yay -Syuq --noconfirm --needed mesa-git gamescope gamemode vulkan-icd-loader vulkan-radeon steam spotify pcsx2-latest-bin komikku krita
+  fi
+
+  echo "Install laptop apps? [Y/n]: "
+  read -n 1 ans
+  if ans != "n" || ans != "N"
+  then
+   yay -Syuq --noconfirm --needed ncspot 
+  fi
+fi
+
 cd ~/ScarletFiles
-stow --dotfiles . 
+stow --dotfiles .
